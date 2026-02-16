27823bbe6f22c7bbf41dbfb1b17e514eab27e119
cheesekaek <karthika.anusurek@gmail.com>
2026-02-16 11:56:54 -0500

    Adden commandlinerunner

--- src/main/java/com/example/pl/PockLedApplication.java
+++ src/main/java/com/example/pl/PockLedApplication.java
@@ -1,11 +1,21 @@
 package com.example.pl;
 
+import com.example.pl.repository.ExpensesRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PockLedApplication {
     public static void main(String[] args) {
         SpringApplication.run(PockLedApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ExpensesRepository expensesRepository) {
+        return args -> {
+
+        };
     }
 }
