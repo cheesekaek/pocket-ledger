7df9381d55377c1259dd7048593c0b29d60500fb
cheesekaek <karthika.anusurek@gmail.com>
2026-02-19 19:26:22 -0500

    class name changed

--- src/main/java/com/example/pl/service/ExpensesService.java
+++ src/main/java/com/example/pl/service/ExpensesService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ExpenseService {
+public class ExpensesService {
 
     @Autowired
     private ExpensesRepository expensesRepository;
