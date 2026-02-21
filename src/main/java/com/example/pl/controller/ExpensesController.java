a83ad1bda2b2b1602277a991a3f8f2ad301a6767
cheesekaek <karthika.anusurek@gmail.com>
2026-02-21 08:19:23 -0500

    new GET method for filtering data through dates

--- src/main/java/com/example/pl/controller/ExpensesController.java
+++ src/main/java/com/example/pl/controller/ExpensesController.java
@@ -1,14 +1,17 @@
 package com.example.pl.controller;
 
 import com.example.pl.dto.ExpensesDto;
+import com.example.pl.exception.InvalidInput;
 import com.example.pl.mapper.ExpensesMapper;
 import com.example.pl.entity.Expenses;
 import com.example.pl.service.ExpensesService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -51,6 +54,35 @@ public class ExpensesController {
     @GetMapping
     public ResponseEntity<List<ExpensesDto>> getExpensesByCategory(String category) {
         List<Expenses> expensesList = expensesService.getExpensesByCategory(category);
+        List<ExpensesDto> expensesDtoList = expensesList.stream().map(expensesMapper::toExpDto).toList();
+        return ResponseEntity.ok(expensesDtoList);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ExpensesDto>> getExpensesByDate(
+            @RequestParam (required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) // conversion from string to date
+            LocalDate dateAfter,
+
+            @RequestParam (required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate dateBefore,
+
+            @RequestParam (required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate date) {
+
+        List<Expenses> expensesList;
+        if (dateAfter != null && dateBefore != null && date == null) {
+            expensesList = expensesService.getExpensesByDateBetween(dateAfter, dateBefore);
+        } else if (date != null && dateAfter == null && dateBefore == null) {
+            expensesList = expensesService.getExpensesByDate(date);
+        } else if (date != null || dateAfter != null || dateBefore != null) {
+            throw new InvalidInput("Either [ both 'dateAfter' and 'dateBefore' ] OR " +
+                    "[ 'date' ] must be provided.");
+        } else {
+            expensesList = expensesService.getAllExpenses();
+        }
         List<ExpensesDto> expensesDtoList = expensesList.stream().map(expensesMapper::toExpDto).toList();
         return ResponseEntity.ok(expensesDtoList);
     }
