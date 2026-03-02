0803ae6022bdf40722be4b6eb308912ca9ade57d
cheesekaek <karthika.anusurek@gmail.com>
2026-03-02 16:47:01 -0500

    new specifications class with filters for category, desc, date(s)

---
+++ src/main/java/com/example/pl/specification/ExpensesSpecification.java
@@ -0,0 +1,33 @@
+package com.example.pl.specification;
+
+import com.example.pl.entity.Expenses;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
+
+public class ExpensesSpecification {
+
+    // filter by category - exact category
+    public static Specification<Expenses> hasCategory(String category) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("category"), category);
+    }
+
+    // filter by desc - similar descriptions
+    public static Specification<Expenses> hasDesc(String description) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.like(criteriaBuilder.lower(root.get(description)), "%" + description + "%");
+    }
+
+    // filter by date - exact date
+    public static Specification<Expenses> hasDate(LocalDate date) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("date"), date);
+    }
+
+    // filter by dates - b/w two dates
+    public static Specification<Expenses> hasDates(LocalDate dateAfter, LocalDate dateBefore) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder.between(root.get("date"), dateAfter, dateBefore);
+    }
+}
